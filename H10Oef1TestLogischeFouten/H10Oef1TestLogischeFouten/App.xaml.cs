11e8e154853a910e977728b8be3405841f0c2314
Indy Iri <indy.iri@student.pxl.be>
2024-11-14 10:46:37 +0100

    WPF aangemaakt

---
+++ H10Oef1TestLogischeFouten/H10Oef1TestLogischeFouten/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace H10Oef1TestLogischeFouten
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
